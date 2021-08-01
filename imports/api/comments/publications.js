a1fc8dd4cb6b9650071cda93a0a907ab97097ea6
Sherin Jacob <sherinjacob20@gmail.com>
2021-08-01 22:38:51 +0530

    Methods and publisher for Comments.

--- imports/api/comments/publications.js
+++ imports/api/comments/publications.js
@@ -2,5 +2,5 @@ import { Meteor } from "meteor/meteor";
 import Comments from "/imports/db/comments/collection";
 
 Meteor.publish("comments", () => {
-  return Comments.find();
+  return Comments.find({});
 });
