d24211a1d851b6d083e63f3e1298a9f78ad6dc0c
Sherin Jacob <sherinjacob20@gmail.com>
2021-08-01 18:58:20 +0530

    Basic functioning form.

--- imports/ui/pages/LoginPage.jsx
+++ imports/ui/pages/LoginPage.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import { Meteor } from "meteor/meteor";
+import React, { useState } from "react";
 
 export const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const clearInputFields = () => {
+    setUserEmail("");
+    setUserPassword("");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(userEmail, userPassword);
+
+    clearInputFields();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="userEmail">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="userEmail"
+            placeHolder="Email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="userEmail">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="userPassword"
+            placeHolder="Password"
+            value={userPassword}
+            onChange={(e) => setUserPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Log In
+          </button>
+        </div>
+        <div></div>
+      </form>
+    </div>
+  );
 };
